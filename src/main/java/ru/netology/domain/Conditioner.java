e14e948a57ca7923210bfff0e8ed02dc31305a3b
Liana Veldiaeva <lianali108@gmail.com>
2020-07-18 15:26:56 +0200

    service add

---
+++ src/main/java/ru/netology/domain/Conditioner.java
@@ -0,0 +1,73 @@
+package ru.netology.domain;
+
+public class Conditioner {
+    private String name;
+    private int maxTemperature;
+    private int minTemperature;
+    private int currentTemperature;
+    private boolean on;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getMaxTemperature() {
+        return maxTemperature;
+    }
+
+    public void setMaxTemperature(int maxTemperature) {
+        this.maxTemperature = maxTemperature;
+    }
+
+    public int getMinTemperature() {
+        return minTemperature;
+    }
+
+    public void setMinTemperature(int minTemperature) {
+        this.minTemperature = minTemperature;
+    }
+
+    public int getCurrentTemperature() {
+        return currentTemperature;
+    }
+
+    public void setCurrentTemperature(int currentTemperature) {
+
+        if (currentTemperature > maxTemperature) {
+            return;
+        }
+        if (currentTemperature < minTemperature) {
+            return;
+        }
+        this.currentTemperature = currentTemperature;
+    }
+
+    public void increaseCurrentTemperature() {
+        int newTemperature = currentTemperature + 1;
+        if (newTemperature > maxTemperature) {
+            return;
+        }
+        this.currentTemperature = newTemperature;
+    }
+
+    public void decreaseCurrentTemperature() {
+        int newTemperature = currentTemperature - 1;
+        if (newTemperature < minTemperature) {
+            return;
+        }
+        this.currentTemperature = newTemperature;
+    }
+
+    public boolean isOn() {
+        return on;
+    }
+    public void setOn(boolean on) {
+        this.on = on;
+    }
+}
+
+
